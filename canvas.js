3b1a85af645a0eca1f8690e3b8a606a8ef110c78
Brandon Thomas Suit <brandon.suit@gmail.com>
2011-08-21 02:30:19 -0400

    Keep track of previous state to avoid redraw

--- canvas.js
+++ canvas.js
@@ -17,6 +17,15 @@ function CameraCanvas(selector)
 	this._width = 0;
 	this._height = 0;
 
+	// Keep track of previously drawn frame state so there is no 
+	// redundant drawing
+	this._previous = {
+		src: null,
+		width: null,
+		height: null,
+		isPaused: null
+	};
+
 	if(typeof(selector) != null) {
 		this.canvas = $(selector)[0];
 		this.context = this.canvas.getContext('2d');
@@ -24,27 +33,74 @@ function CameraCanvas(selector)
 
 	this.resize = function(width, height)
 	{
-		this._width = width;
-		this._height = height;
+		// Changing size forces redraw, so don't set unless size changed	
+		if(this._width != width || this._height != height) 
+		{
+			this._width = width;
+			this._height = height;
 
-		this.canvas.width = width;
-		this.canvas.height = height;
+			this.canvas.width = width;
+			this.canvas.height = height;
+		}
 
 		this.draw();
+	}
+
+	this.hasChanged = function()
+	{
+		var source = this.defaultImage;
+
+		if(this.camera.curFrame && this.camera.curFrame.src) {
+			source = this.camera.curFrame.src;
+		}
+
+		if(this._width != this._previous.width || 
+				this._height != this._previous.height) {
+			return true;
+		}
+
+		if(this.camera.isPaused != this._previous.isPaused) {
+			return true;
+		}
+
+		// If paused, we don't care about the source being updated
+		if(this.camera.isPaused) {
+			return false;
+		}
+
+		if(source != this._previous.src) {
+			return true;
+		}
+
+		return false;
 	}
 
 	this.draw = function()
 	{
 		var ctx = this.canvas.getContext('2d');
 		var source = this.defaultImage;
-
-		// TODO: 'needs redraw' flag
+		var doClear = true;
 
 		if(this.camera.curFrame && this.camera.curFrame.src) {
 			source = this.camera.curFrame;
+			doClear = false;
 		}
 
+		// Can we skip drawing?
+		if(!this.hasChanged()) {
+			return;
+		}
+
+		// Update previous state
+		this._previous.width = this._width;
+		this._previous.height = this._height;
+		this._previous.src = source;
+		this._previous.isPaused = this.camera.isPaused;
+
 		try {
+			if(doClear) {
+				ctx.clearRect(0, 0, this._width, this._height);
+			}
 			ctx.drawImage(source, 0, 0, this._width, this._height);
 		}
 		catch(e) {
